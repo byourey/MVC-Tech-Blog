528c8f2a7c4da07ff6cd087ba815ce39be4ffa51
Bjorn Yourey <byourey7783@gmail.com>
2021-03-22 20:02:21 -0400

    added files and folders with data

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const db = require('../config/database');
+
+
+// Add a post
+router.get('/addblog', (req, res) => {
+    const data ={
+        title: 'Why I like coding',
+        description: 'Just like architecture, which is an art used by people daily and affects their everyday life, so is code. You can create something and people interact with, use, touch and work with. That is an amazing feeling.'
+        
+    }
+
+    let { title, description } = data;
+
+// Insert into table
+Blog.create({
+    title: title,
+    description: description
+     }).then(())
+     .catch((err) => console.log(err));
+
+});
+
+
+module.exports = router;
