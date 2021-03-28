d41cb664c3f86d6f9c7784f59b7270d4c1ddeb80
Bjorn Yourey <byourey7783@gmail.com>
2021-03-28 18:18:11 -0400

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # MVC-Tech-Blog
+
+https://mvctechblog.herokuapp.com/
+
+## Description
+A tech blog built using handlebars, Express, Sequelize, Bulma. You can create , add, update and delete blog posts.
+
+## Install
+Clone repository
+Open CLI
+Run npm install
+Open url to add , create, update and delete posts
+
+## Technologies
+Node.js
+NPM
+MySQL
+Express
+Bulma
+
+
+
+
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -56,8 +56,7 @@ router.post('/', (req, res) => {
       username: req.body.username,
       email: req.body.email,
       password: req.body.password,
-      twitter: req.body.twitter,
-      github: req.body.github
+      
     })
     .then(dbUserData => {
       req.session.save(() => {
@@ -70,89 +69,6 @@ router.post('/', (req, res) => {
     });
   });
 
-  // LOGIN
-  router.post('/login', (req, res) => {
-    User.findOne({
-      where: {
-        email: req.body.email
-      }
-    }).then(dbUserData => {
-      if (!dbUserData) {
-        res.status(400).json({ message: 'No email address found' });
-        return;
-      }
-  
-      const valPassword = dbUserData.checkPassword(req.body.password);
-  
-      if (!valPassword) {
-        res.status(400).json({ message: 'Wrong password!' });
-        return;
-      }
-  
-      req.session.save(() => {
-        // declare session variables
-        req.session.user_id = dbUserData.id;
-        req.session.username = dbUserData.username;
-        req.session.twitter = dbUserData.twitter;
-        req.session.github = dbUserData.github;
-        req.session.loggedIn = true;
-  
-        res.json({ user: dbUserData, message: 'You are now logged in!' });
-      });
-    });
-  });
 
-
-  router.post('/logout', (req, res) => {
-    if (req.session.loggedIn) {
-      req.session.destroy(() => {
-        res.status(204).end();
-      });
-    }
-    else {
-      res.status(404).end();
-    }
-  });
-
-// PUT /api/users/1
-router.put('/:id', withAuth, (req, res) => {
-    User.update(req.body, {
-        individualHooks: true,
-        where: {
-            id: req.params.id
-      }
-    })
-      .then(dbUserData => {
-        if (!dbUserData[0]) {
-          res.status(404).json({ message: 'No user found with this id' });
-          return;
-        }
-        res.json(dbUserData);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
-
-// DELETE /api/users/1
-router.delete('/:id', withAuth, (req, res) => {
-    User.destroy({
-      where: {
-        id: req.params.id
-      }
-    })
-      .then(dbUserData => {
-        if (!dbUserData) {
-          res.status(404).json({ message: 'No user found with this id' });
-          return;
-        }
-        res.json(dbUserData);
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
 
 module.exports = router;
