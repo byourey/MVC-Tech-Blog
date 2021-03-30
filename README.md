09172590165105a9900a193c3283763f6f57a312
Bjorn Yourey <byourey7783@gmail.com>
2021-03-29 21:18:41 -0400

    made changes

--- README.md
+++ README.md
@@ -6,17 +6,17 @@ https://mvctechblog.herokuapp.com/
 A tech blog built using handlebars, Express, Sequelize, Bulma. You can create , add, update and delete blog posts.
 
 ## Install
-Clone repository
-Open CLI
-Run npm install
-Open url to add , create, update and delete posts
+* Clone repository
+* Open CLI
+* Run npm install
+* Open url to add , create, update and delete posts
 
 ## Technologies
-Node.js
-NPM
-MySQL
-Express
-Bulma
+* Node.js
+* NPM
+* MySQL
+* Express
+
 
 
 
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,30 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+
 
 router.get('/', async (req, res) => {
   // I'm able to get this route working only for now
   res.render('homepage');
 });
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('signup');
+});
+
+
 
 module.exports = router;
